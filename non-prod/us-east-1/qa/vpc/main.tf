7deac9d5ea0df528be2bfe3ff59d441abe937450
jiangguoqing <mrjiangguoqing@gmail.com>
2023-06-18 19:20:39 +0800

    test

--- non-prod/us-east-1/qa/vpc/main.tf
+++ non-prod/us-east-1/qa/vpc/main.tf
@@ -23,7 +23,7 @@ locals {
 ################################################################################
 
 module "vpc" {
-  source = "git::git@github.com:jiangguoqing/iac-x.git//vpc"
+  source = "git::github.com:jiangguoqing/iac-x.git//vpc"
 
   name = local.name
   cidr = local.vpc_cidr
